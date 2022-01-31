46337922565322bcdb22580551916a245ed870bb
fumito-s <fmt.fumito@gmail.com>
2022-01-31 21:40:54 +0900

    Initial commit

---
+++ app/src/main/java/com/fumito/statistics/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.fumito.statistics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.fumito.statistics.ui.theme.StatisticsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StatisticsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StatisticsTheme {
+        Greeting("Android")
+    }
+}
