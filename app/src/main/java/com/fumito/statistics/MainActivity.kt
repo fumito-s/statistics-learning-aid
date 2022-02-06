139de7d86d7ab9f1ec3e732ed74da040d998672e
fumito-s <fmt.fumito@gmail.com>
2022-02-06 18:40:34 +0900

    Add Scafforld Composable

--- app/src/main/java/com/fumito/statistics/MainActivity.kt
+++ app/src/main/java/com/fumito/statistics/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import com.fumito.statistics.ui.screen.MainScreen
 import com.fumito.statistics.ui.theme.StatisticsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StatisticsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                }
-            }
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    StatisticsTheme {
+        Scaffold {
+            MainScreen()
         }
     }
 }
