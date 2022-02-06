a0f6ecdfa0a5c5d202ca6e70e3c833e0bd22f969
fumito-s <fmt.fumito@gmail.com>
2022-02-06 18:43:38 +0900

    Add Naviagtion

--- app/src/main/java/com/fumito/statistics/MainActivity.kt
+++ app/src/main/java/com/fumito/statistics/MainActivity.kt
@@ -3,10 +3,11 @@ package com.fumito.statistics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.fumito.statistics.ui.screen.MainScreen
 import com.fumito.statistics.ui.theme.StatisticsTheme
 
@@ -22,8 +23,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     StatisticsTheme {
+        val navController = rememberNavController()
         Scaffold {
-            MainScreen()
+            NavHost(navController = navController, startDestination = "main") {
+                composable("main") {
+                    MainScreen()
+                }
+            }
         }
     }
 }
